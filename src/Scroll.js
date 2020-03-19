c0190b28651334c6e435be4f0232dda59322ff6b
MaartenQHuijser <maarten@huijser.nl>
2020-03-19 09:14:18 +0100

    Add: scroll component

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 const Scroll = (props) => {
-    return props.children
+    return (
+        <div style={{ overflowY: 'scroll', border: '2px solid black', height: '500px' }}>
+            {props.children}
+        </div>
+    )
 }
 
 export default Scroll;
